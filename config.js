e70c728dca182aec8b27ee33273dfbe7fe9827a4
Milan Gorislavets <72350558+JaerYT@users.noreply.github.com>
2022-11-17 16:58:38 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "AllayLand";
+var server_ip = "play.allayland.fun";
+var server_port = 25565;
+var ip_copied = "Скопировано!";
+
+var firefly_count = 20;
+
+staff("_sztorm", "6625e164-9a09-4195-93cd-4d10d01f4379", "Создатель");
+staff("Jaer1337", "31ff4ca3-5189-48c3-8573-568dbc5ccb2e", "Разработчик/вебмастер");
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
